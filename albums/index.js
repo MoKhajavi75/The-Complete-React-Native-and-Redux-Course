e1718e6299ec894bbeb942ba40210b22475296b2
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-04-27 21:48:19 +0430

    Register a component

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from "react";
+import { Text, AppRegistry } from "react-native";
 
-AppRegistry.registerComponent('albums', () => App);
+const App = () => <Text>Some Text!</Text>;
+
+AppRegistry.registerComponent("albums", () => App);
