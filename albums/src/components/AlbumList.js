2dae38c86ff49ac92983dbf9a2b1cc9458561b95
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-03 22:56:33 +0430

    Refactor AlbumList

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { View } from "react-native";
+import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>List!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
+  }
+
+  render() {
+    return <View>{this.renderAlbums()}</View>;
+  }
+}
 
 export default AlbumList;
