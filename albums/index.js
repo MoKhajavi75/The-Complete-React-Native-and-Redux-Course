8c784425b56e2e56d9d099d6bf35a702a17ad5da
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-03 17:07:14 +0430

    Minor Change

--- albums/index.js
+++ albums/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppRegistry } from "react-native";
-import Header from "./src/components/header";
+import Header from "./src/components/Header";
 
 const App = () => <Header headerText={"!"} />;
 
