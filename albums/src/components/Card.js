b0dcfdb366e6f02cc31cfaa5daa39cf6a94f7d59
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-03 23:48:54 +0430

    Cleanups!

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { View } from "react-native";
 
-const Card = () => {
-  return (
-    <View style={styles.containterStyle}>
-      <Text>test</Text>
-    </View>
-  );
+const Card = props => {
+  return <View style={styles.containterStyle}>{props.children}</View>;
 };
-
-export default Card;
 
 const styles = {
   containterStyle: {
@@ -17,9 +11,11 @@ const styles = {
     borderRadius: 2,
     borderColor: "#D0D0D0",
     borderBottomWidth: 0,
-    elevation: 3,
+    elevation: 1,
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10
   }
 };
+
+export default Card;
