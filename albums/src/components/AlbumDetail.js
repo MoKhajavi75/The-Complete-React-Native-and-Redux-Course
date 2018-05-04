d48fd5f8acf24a168c74fc4ea55e8317b1110ebf
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-05 00:59:38 +0430

    Add "Buy Now" Button

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Linking } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
+import Button from "./Button";
 
 const AlbumDetail = props => {
   return (
@@ -22,6 +23,12 @@ const AlbumDetail = props => {
 
       <CardSection>
         <Image style={styles.ImageStyle} source={{ uri: props.album.image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(props.album.url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
