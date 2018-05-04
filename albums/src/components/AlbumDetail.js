1791fa6b8c86dea6eee8bdd2a0641f5c100d6623
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-04 23:59:38 +0430

    Add more items!

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
 
@@ -7,14 +7,49 @@ const AlbumDetail = props => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={styles.thumbnailContainer}>
+          <Image
+            style={styles.thumbnail}
+            source={{ uri: props.album.thumbnail_image }}
+          />
+        </View>
+
+        <View style={styles.headerContent}>
+          <Text style={styles.headerText}>{props.album.title}</Text>
+          <Text>{props.album.artist}</Text>
+        </View>
       </CardSection>
 
       <CardSection>
-        <Text>{props.album.artist}</Text>
+        <Image style={styles.ImageStyle} source={{ uri: props.album.image }} />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContent: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  headerText: {
+    fontSize: 18
+  },
+  thumbnail: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  ImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
