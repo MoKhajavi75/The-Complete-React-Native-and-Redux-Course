5b0d8918684fa16c1104315e047f171edd5d0b8a
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-18 01:27:09 +0430

    Add Firebase
    1. https://github.com/firebase/firebase-js-sdk
    
    2. i'm in Iran, so use npm proxy :neutral_face:

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import firebase from "firebase";
 import { Header } from "./components/common";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDZdd-5u10po03Yu4f48b7IIs4F2y6n5Vs",
+      authDomain: "authentication-f6711.firebaseapp.com",
+      databaseURL: "https://authentication-f6711.firebaseio.com",
+      projectId: "authentication-f6711",
+      storageBucket: "authentication-f6711.appspot.com",
+      messagingSenderId: "1058681011520"
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>Testing!</Text>
+        <LoginForm />
       </View>
     );
   }
