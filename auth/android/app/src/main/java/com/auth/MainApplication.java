461f8a6e2e09b6eb5cd7acc9689db43c310fe38b
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-18 00:54:33 +0430

    Revert "Add RNFirebase"
    This reverts commit e5bf85e3204b44e5adeba95fd4f2c70c089d2a90.

--- auth/android/app/src/main/java/com/auth/MainApplication.java
+++ auth/android/app/src/main/java/com/auth/MainApplication.java
@@ -3,7 +3,6 @@ package com.auth;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFirebasePackage()
+          new MainReactPackage()
       );
     }
 
