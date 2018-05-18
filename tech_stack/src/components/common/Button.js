27b0f9248ed4ddb6d04a0a5a8c67fe26c156545a
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-18 20:21:00 +0430

    Add common components

---
+++ tech_stack/src/components/common/Button.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+const Button = props => {
+  return (
+    <TouchableOpacity style={styles.buttonStyle} onPress={props.onPress}>
+      <Text style={styles.textStyle}>{props.children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: "stretch",
+    backgroundColor: "#F0F0F0",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#007AFF",
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: "center",
+    color: "#007AFF",
+    fontSize: 16,
+    fontWeight: "600",
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
+
+export { Button };
---
+++ tech_stack/src/components/common/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View } from "react-native";
+
+const Card = props => {
+  return <View style={styles.containerStyle}>{props.children}</View>;
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "#D0D0D0",
+    borderBottomWidth: 0,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
+  }
+};
+
+export { Card };
