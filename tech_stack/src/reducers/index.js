8cb80e7bb5975851a38d8adac4dae3a10bfedbfb
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-23 13:39:20 +0430

    Fix Little nasty bug :neutral_face:

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -4,5 +4,5 @@ import SelectionReducer from "./SelectionReducer";
 
 export default combineReducers({
   libraries: LibraryReducer,
-  SelectedLibraryId: SelectionReducer
+  selectedLibraryId: SelectionReducer
 });
