c65085ffef5d2e1b865ecf891c82c27492705192
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-23 14:00:04 +0430

    Refactor (move logic out of component)

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -6,7 +6,7 @@ import * as actions from "../actions";
 
 class ListItem extends Component {
   renderDescription() {
-    if (this.props.library.id === this.props.selectedLibraryId) {
+    if (this.props.expanded) {
       return <Text>{this.props.library.description}</Text>;
     }
   }
@@ -35,10 +35,9 @@ const styles = {
   }
 };
 
-const mapStateToProps = state => {
-  return {
-    selectedLibraryId: state.selectedLibraryId
-  };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
