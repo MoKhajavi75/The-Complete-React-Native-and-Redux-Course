6b831648fab08753a71c2bbd580e12d935a48ad3
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-25 02:31:24 +0430

    Add Animation :grin:
    Note that in order to get this to work on Android you need to set the following flags via UIManager:
    
    UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import { Text, TouchableWithoutFeedback, View } from "react-native";
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation,
+  UIManager
+} from "react-native";
 import { connect } from "react-redux";
 import { CardSection } from "./common";
 import * as actions from "../actions";
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
     if (this.props.expanded) {
       return (
