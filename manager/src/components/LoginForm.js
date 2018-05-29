a970d215f806af6f3ad1c827655a771b87806d83
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-05-29 15:42:40 +0430

    Connect

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { emailChanged } from "../actions";
 import { Card, CardSection, Input, Button } from "./common";
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="email@gmail.com" />
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+          />
         </CardSection>
 
         <CardSection>
@@ -21,4 +31,4 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
