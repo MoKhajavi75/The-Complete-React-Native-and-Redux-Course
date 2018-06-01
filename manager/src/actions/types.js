ad22572b98fd8a716d9b3f30d3cc63ba8c743ac0
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-01 20:09:35 +0430

    Fix misspelling

--- manager/src/actions/types.js
+++ manager/src/actions/types.js
@@ -4,4 +4,4 @@ export const LOGIN_USER = "login_user";
 export const LOGIN_USER_SUCCESS = "login_user_success";
 export const LOGIN_USER_FAIL = "login_user_fail";
 
-export const EMPLOYEE_UPDATE = "eployee_update";
+export const EMPLOYEE_UPDATE = "employee_update";
