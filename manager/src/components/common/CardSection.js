ece3b0820339b69ffa9268514ebd1787353b5138
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-01 21:10:07 +0430

    Add style prop

--- manager/src/components/common/CardSection.js
+++ manager/src/components/common/CardSection.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Text, View } from "react-native";
 
 const CardSection = props => {
-  return <View style={styles.containterStyle}>{props.children}</View>;
+  return (
+    <View style={[styles.containerStyle, props.style]}>{props.children}</View>
+  );
 };
 
 const styles = {
-  containterStyle: {
+  containerStyle: {
     borderBottomWidth: 1,
     padding: 5,
     backgroundColor: "#F0F0F0",
