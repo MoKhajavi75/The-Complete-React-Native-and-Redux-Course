703c5c05b8c01f4de5a262ab408d3412f8e6ff55
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-01 06:35:17 +0430

    Fix react-native-router-flux android bug

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -45,30 +45,32 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <Card>
-        <CardSection>
-          <Input
-            label="Email"
-            placeholder="email@gmail.com"
-            onChangeText={this.onEmailChange.bind(this)}
-            value={this.props.email}
-          />
-        </CardSection>
+      <View style={{ marginTop: 50 }}>
+        <Card>
+          <CardSection>
+            <Input
+              label="Email"
+              placeholder="email@gmail.com"
+              onChangeText={this.onEmailChange.bind(this)}
+              value={this.props.email}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input
-            secureTextEntry
-            label="Password"
-            placeholder="password"
-            onChangeText={this.onPasswordChange.bind(this)}
-            value={this.props.password}
-          />
-        </CardSection>
+          <CardSection>
+            <Input
+              secureTextEntry
+              label="Password"
+              placeholder="password"
+              onChangeText={this.onPasswordChange.bind(this)}
+              value={this.props.password}
+            />
+          </CardSection>
 
-        {this.renderError()}
+          {this.renderError()}
 
-        <CardSection>{this.renderButton()}</CardSection>
-      </Card>
+          <CardSection>{this.renderButton()}</CardSection>
+        </Card>
+      </View>
     );
   }
 }
