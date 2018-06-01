9ad17658b9bc7e25ae45a140a52e1bc543fb8337
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-01 16:53:11 +0430

    Add EmployeeList

---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee List!</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
