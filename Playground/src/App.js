00766d12ffa084411e54482c7651b0e3082bd90f
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-15 05:04:27 +0430

    Ans: different style for last element
    https://stackoverflow.com/questions/50847577/why-does-react-js-not-offer-a-justify-self

--- Playground/src/App.js
+++ Playground/src/App.js
@@ -1,26 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-export default class App extends Component {
+class Playground extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.boxes} />
+        <View style={styles.boxes} />
+        <View
+          style={[
+            styles.boxes,
+            {
+              backgroundColor: "crimson",
+              position: "absolute",
+              right: 0
+            }
+          ]}
+        />
       </View>
     );
   }
@@ -29,18 +25,16 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  boxes: {
+    width: 50,
+    height: 50,
+    marginLeft: 1, // to separate each box!
+    backgroundColor: "steelblue"
   }
 });
+
+export default Playground;
