3ba4f08cce9d509026e5098d3deabf7892c78ffa
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-16 15:57:59 +0430

    Cleanups!

--- Playground/src/App.js
+++ Playground/src/App.js
@@ -1,40 +1,25 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>
-          <Text>Lorem Ipsum is </Text>
-          <Text style={styles.emptyTermStyle}> </Text>
-          <Text>
-            {" "}
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book
-          </Text>
+          <Text>First</Text>
+          <Text style={{ borderBottomWidth: 1 }}>Second</Text>
+          <Text>Third</Text>
         </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    padding: 25,
-    paddingTop: 20,
-    backgroundColor: "#ecf0f1"
-  },
-
-  emptyTermStyle: {
-    width: 50,
-    borderBottomWidth: 1,
-    marginVertical: 5
+    backgroundColor: "#ECF0F1"
   }
-});
+};
