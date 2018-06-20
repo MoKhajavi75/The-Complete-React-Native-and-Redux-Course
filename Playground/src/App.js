2535784805ab72c4c4e2025e728f92e96e0b7126
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-20 18:50:12 +0430

    Ans:
    https://stackoverflow.com/questions/50884639/empty-text-space-in-a-continuous-text-component-react-native

--- Playground/src/App.js
+++ Playground/src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Constants } from "expo";
+
+splitPhrase = (phrase, isTerm = false) => {
+  let words = phrase.split(" ");
+  return words.map((i, k) => {
+    return (
+      <Text key={k} style={isTerm ? styles.emptyTerm : styles.paragraphs}>
+        {" "}
+        {i}{" "}
+      </Text>
+    );
+  });
+};
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          <Text>First</Text>
-          <Text style={{ borderBottomWidth: 1 }}>Second</Text>
-          <Text>Third</Text>
-        </Text>
+        <View style={styles.paragraphs}>
+          <Text style={styles.textParagraph}>Lorem Ipsum is</Text>
+          <Text style={styles.emptyTerm} />
+          <Text style={styles.textParagraph}>
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#ECF0F1"
+    padding: 25,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1"
+  },
+  emptyTerm: {
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    minWidth: "25%"
+  },
+  paragraphs: {
+    flexDirection: "row",
+    flexWrap: "wrap"
   }
-};
+});
