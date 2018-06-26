a8a8fcadf04199028fdafa464f869322c036de1d
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-26 14:00:18 +0430

    Add 4 simple buttons

--- Playground/src/App.js
+++ Playground/src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        {/* top */}
+        <View style={styles.topContainer}>
+          <Text style={styles.topTextContainer}>Top</Text>
+        </View>
+
+        {/* bottom */}
+        <View style={styles.bottomContainer}>
+          {/* left */}
+          <View style={styles.sideContainer}>
+            <TouchableOpacity style={styles.numericButton}>
+              <Text style={styles.buttonText}>left 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.numericButton}>
+              <Text style={styles.buttonText}>left 2</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* right */}
+          <View style={styles.sideContainer}>
+            <TouchableOpacity style={styles.numericButton}>
+              <Text style={styles.buttonText}>right 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.numericButton}>
+              <Text style={styles.buttonText}>right 2</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -16,7 +42,53 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#ecf0f1"
+    backgroundColor: "#F0FFFF",
+    padding: 5
+  },
+
+  topContainer: {
+    flex: 3,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    backgroundColor: "#252525"
+  },
+
+  topTextContainer: {
+    alignSelf: "center",
+    color: "#F8F8FF"
+  },
+
+  bottomContainer: {
+    flex: 7,
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "center",
+    backgroundColor: "#D3D3D3"
+  },
+
+  sideContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  numericButton: {
+    flex: 1,
+    backgroundColor: "#B0C4DE",
+    alignSelf: "stretch",
+    justifyContent: "center",
+    margin: 5
+  },
+
+  arithmeticButton: {
+    backgroundColor: "#FF8C00",
+    alignItems: "center",
+    margin: 20
+  },
+
+  buttonText: {
+    textAlign: "center"
   }
 });
 
