98d0998c34404dccf7c0bd643ca186c450c33e3d
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2018-06-26 20:17:27 +0430

    PanResponder example

--- Playground/src/App.js
+++ Playground/src/App.js
@@ -1,116 +1,76 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  StatusBar,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  PanResponder,
+  TouchableOpacity,
+  Dimensions
+} from "react-native";
 
-class App extends Component {
+const { width, height } = Dimensions.get("window");
+
+const getDirectionAndColor = ({ moveX, moveY, dx, dy }) => {
+  const draggedDown = dy > 30;
+  const draggedUp = dy < -30;
+  const draggedLeft = dx < -30;
+  const draggedRight = dx > 30;
+  const isRed = moveY < 90 && moveY > 40 && moveX > 0 && moveX < width;
+  const isBlue = moveY > height - 50 && moveX > 0 && moveX < width;
+  let dragDirection = "";
+
+  if (draggedDown || draggedUp) {
+    if (draggedDown) dragDirection += "dragged down ";
+    if (draggedUp) dragDirection += "dragged up ";
+  }
+
+  if (draggedLeft || draggedRight) {
+    if (draggedLeft) dragDirection += "dragged left ";
+    if (draggedRight) dragDirection += "dragged right ";
+  }
+
+  if (isRed) return `red ${dragDirection}`;
+  if (isBlue) return `blue ${dragDirection}`;
+  if (dragDirection) return dragDirection;
+};
+
+export default class App extends Component {
+  state = {
+    zone: "Still Touchable"
+  };
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) =>
+        !!getDirectionAndColor(gestureState),
+      onPanResponderMove: (evt, gestureState) => {
+        const drag = getDirectionAndColor(gestureState);
+        this.setState({
+          zone: drag
+        });
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true
+    });
+  }
+
+  onPress = () => {
+    this.setState({
+      zone: "I got touched with a parent pan responder"
+    });
+  };
+
   render() {
-    const {
-      container,
-      topContainer,
-      topTextContainer,
-      bottomContainer,
-      buttonContainer,
-      rowStyles,
-      commandButton,
-      numericButton,
-      arithmeticButton,
-      buttonText
-    } = styles;
-
     return (
-      <View style={container}>
-        {/* top */}
-        <View style={topContainer}>
-          <Text style={topTextContainer}>Top</Text>
+      <View style={styles.container} {...this._panResponder.panHandlers}>
+        <StatusBar hidden />
+        <View style={styles.zone1} />
+        <View style={styles.center}>
+          <TouchableOpacity onPress={this.onPress}>
+            <Text>{this.state.zone}</Text>
+          </TouchableOpacity>
         </View>
-
-        {/* bottom */}
-        <View style={bottomContainer}>
-          {/* left */}
-          <View style={buttonContainer}>
-            {/* 1st row */}
-            <View style={rowStyles}>
-              <TouchableOpacity style={commandButton}>
-                <Text style={buttonText}>CA</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={commandButton}>
-                <Text style={buttonText}>+/-</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={commandButton}>
-                <Text style={buttonText}>%</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={arithmeticButton}>
-                <Text style={buttonText}>÷</Text>
-              </TouchableOpacity>
-            </View>
-
-            {/* 2nd row */}
-            <View style={rowStyles}>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>7</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>8</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>9</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={arithmeticButton}>
-                <Text style={buttonText}>×</Text>
-              </TouchableOpacity>
-            </View>
-
-            {/* 3rd row */}
-            <View style={rowStyles}>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>4</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>5</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>6</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={arithmeticButton}>
-                <Text style={buttonText}>-</Text>
-              </TouchableOpacity>
-            </View>
-
-            {/* 4th row */}
-            <View style={rowStyles}>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>1</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>2</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={numericButton}>
-                <Text style={buttonText}>3</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={arithmeticButton}>
-                <Text style={buttonText}>+</Text>
-              </TouchableOpacity>
-            </View>
-
-            {/* 5th row */}
-            <View style={rowStyles}>
-              <View style={{ flex: 2, flexDirection: "row" }}>
-                <TouchableOpacity style={numericButton}>
-                  <Text style={buttonText}>0</Text>
-                </TouchableOpacity>
-              </View>
-              <View style={rowStyles}>
-                <TouchableOpacity style={numericButton}>
-                  <Text style={buttonText}>.</Text>
-                </TouchableOpacity>
-              </View>
-              <View style={rowStyles}>
-                <TouchableOpacity style={arithmeticButton}>
-                  <Text style={buttonText}>=</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-        </View>
+        <View style={styles.zone2} />
       </View>
     );
   }
@@ -118,77 +78,27 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  center: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#F0FFFF",
-    padding: 5
-  },
-
-  topContainer: {
-    flex: 3,
-    alignSelf: "stretch",
-    justifyContent: "center",
-    backgroundColor: "#252525"
-  },
-
-  topTextContainer: {
-    alignSelf: "center",
-    color: "#F8F8FF"
-  },
-
-  bottomContainer: {
-    flex: 7,
-    flexDirection: "row",
-    alignSelf: "stretch",
-    justifyContent: "center",
-    backgroundColor: "#D3D3D3"
-  },
-
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "stretch",
     justifyContent: "center"
   },
-
-  rowStyles: {
-    flex: 1,
-    flexDirection: "row"
+  zone1: {
+    top: 40,
+    left: 0,
+    right: 0,
+    height: 50,
+    position: "absolute",
+    backgroundColor: "red"
   },
-
-  numericButton: {
-    flex: 1,
-    alignSelf: "stretch",
-    justifyContent: "center",
-    backgroundColor: "#B0C4DE",
-    margin: 5,
-    borderRadius: 5
-  },
-
-  arithmeticButton: {
-    flex: 1,
-    alignSelf: "stretch",
-    justifyContent: "center",
-    backgroundColor: "#FF9C00",
-    margin: 5,
-    borderRadius: 5
-  },
-
-  commandButton: {
-    flex: 1,
-    alignSelf: "stretch",
-    justifyContent: "center",
-    backgroundColor: "#778899",
-    margin: 5,
-    borderRadius: 5
-  },
-
-  buttonText: {
-    textAlign: "center",
-    fontSize: 20,
-    fontWeight: "bold"
+  zone2: {
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 50,
+    position: "absolute",
+    backgroundColor: "blue"
   }
 });
-
-export default App;
